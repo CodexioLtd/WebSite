886656f740311c52778e9b3e9dfe4771b7daaf37
nikoletaogoryalkova <nikoletaogoryalkova@gmail.com>
2018-06-12 17:00:45 +0300

    section-Codexio-info

--- dom.js
+++ dom.js
@@ -4,42 +4,4 @@ function append() {
     pfirst
         .addClass("p-text-first")
         .appendTo(".article-web")
-
-    let psecond = $("<p>");
-    psecond
-        .addClass("p-text-second")
-        .appendTo(".article-web")
-
-    let pthird = $("<p>");
-    pthird
-        .addClass("p-text-third")
-        .appendTo(".article-mobile")
-
-    let pfourth = $("<p>");
-    pfourth
-        .addClass("p-text-fourth")
-        .appendTo(".article-mobile")
-
-    let pfifth = $("<p>");
-    pfifth
-        .addClass("p-text-fifth")
-        .appendTo(".article-blockchain")
-
-    let psixth = $("<p>");
-    psixth
-        .addClass("p-text-sixth")
-        .appendTo(".article-blockchain")
-
-
-    let pseventh = $("<p>");
-    pseventh
-        .addClass("p-text-seventh")
-        .appendTo(".article-consultancy")
-
-    let peighth = $("<p>");
-    peighth
-        .addClass("p-text-eighth")
-        .appendTo(".article-consultancy")
-
-
 };
