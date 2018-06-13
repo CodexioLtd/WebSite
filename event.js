d163bd1e458951c03609878d1cf3a9d37d455289
RoYaLBG <royal4o@gmail.com>
2018-06-13 20:35:45 +0300

    paragraph distiction with hover effect on services section

--- event.js
+++ event.js
@@ -1,12 +1,12 @@
 function ClickWeb() {
-    $('#web').click(function () {
+    $('#web').mouseover(function () {
 
         $('.heading-description').text(`Web Development`);
-        $('.service-description').text(`Most of you might not remember, but the software was once shipped physically by discettes and discs and then installed 
+        $('.service-description').html(`<p class="service-description-paragraph">Most of you might not remember, but the software was once shipped physically by discettes and discs and then installed 
         on the user's computer. Although there is a decent amount of software still installed on our computers we used to make our day-to-day operations 
-        by using web applications. 
-        We have specialized in creating web applications of any scale capable of serving to any amount of users simultaneously. From simple informational
-         web sites (e.g. landing pages, business web sites) to complex applications running in the web browser (e.g. ERP, CRM). `);
+        by using web applications. </p>
+        <p class="service-description-paragraph">We have specialized in creating web applications of any scale capable of serving to any amount of users simultaneously. From simple informational
+         web sites (e.g. landing pages, business web sites) to complex applications running in the web browser (e.g. ERP, CRM). </p>`);
          $('#web-img').css("opacity","1");
          $('#mobile-img').css("opacity","0.5");
          $('#blockchain-img').css("opacity","0.5");
@@ -16,13 +16,13 @@ function ClickWeb() {
 };
 
 function ClickMobile() {
-    $('#mobile').click(function () {
+    $('#mobile').mouseover(function () {
 
         $('.heading-description').text(`Mobile Development`);
-        $('.service-description').text(`What time is it? Correct. You've just looked at your phone to check it. Admit it, we are using our mobile phones everyda 
-         more than anything else. If something can run on your mobile phone, it will eventually run.
-        Creating any service or informational application for mobile is our "Good morning!" phrase. That’s why it is very important to us to be able to run 
-        in one and the same way on all of the major mobile operating systems.`);
+        $('.service-description').html(`<p class="service-description-paragraph">What time is it? Correct. You've just looked at your phone to check it. Admit it, we are using our mobile phones everyda 
+         more than anything else. If something can run on your mobile phone, it will eventually run.</p>
+        <p class="service-description-paragraph">Creating any service or informational application for mobile is our "Good morning!" phrase. That’s why it is very important to us to be able to run 
+        in one and the same way on all of the major mobile operating systems.</p>`);
         $('#mobile-img').css("opacity","1");
         $('#web-img').css("opacity","0.5");
         $('#blockchain-img').css("opacity","0.5");
@@ -32,13 +32,13 @@ function ClickMobile() {
 };
 
 function ClickBlockchain() {
-    $('#blockchain').click(function () {
+    $('#blockchain').mouseover(function () {
 
         $('.heading-description').text(`Bloсkchain Development`);
-        $('.service-description').text(`Economic models nowadays have become way too complex. Middlemen are everywhere. Validation by authorities. Trust. 
-        But the way we work removes the need for a middleman, leaving just the peer to peer communication. 
-        Creating decentralized applications, smart contracts or running a new blockchain network are just a few of the services we offer in this area. 
-        We have proven our skills by creating the biggest platform in the travelling industry purely relying on blockchain operations.`);
+        $('.service-description').html(`<p class="service-description-paragraph">Economic models nowadays have become way too complex. Middlemen are everywhere. Validation by authorities. Trust. 
+        But the way we work removes the need for a middleman, leaving just the peer to peer communication.</p>
+        <p class="service-description-paragraph">Creating decentralized applications, smart contracts or running a new blockchain network are just a few of the services we offer in this area. 
+        We have proven our skills by creating the biggest platform in the travelling industry purely relying on blockchain operations.</p>`);
         $('#blockchain-img').css("opacity","1");
         $('#web-img').css("opacity","0.5");
         $('#mobile-img').css("opacity","0.5");
@@ -48,13 +48,13 @@ function ClickBlockchain() {
 };
 
 function ClickConsultancy() {
-    $('#consultancy').click(function () {
+    $('#consultancy').mouseover(function () {
 
         $('.heading-description').text(`Consultancy`);
-        $('.service-description').text(`You are already working with an existing software development team? Are they hard to organize? 
-        Are they facing showstoppers? Or their power is not enough for the project's needs? We are here to not only solve all of these problems 
-        but also many more others that may arise. We can plug into an existing team and solve organizational or technical problems by providing innovative
-        solutions. We can follow the existing process and suggest improvements, because we believe in the procedural way of working.`);
+        $('.service-description').html(`<p class="service-description-paragraph">You are already working with an existing software development team? Are they hard to organize? 
+        Are they facing showstoppers? Or their power is not enough for the project's needs?</p>
+        <p class="service-description-paragraph">We are here to not only solve all of these problems but also many more others that may arise. We can plug into an existing team and solve organizational or technical problems by providing innovative
+        solutions. We can follow the existing process and suggest improvements, because we believe in the procedural way of working.</p>`);
         $('#consultancy-img').css("opacity","1");
         $('#web-img').css("opacity","0.5");
         $('#mobile-img').css("opacity","0.5");
