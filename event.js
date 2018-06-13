e46349793b6c35b8243106d57c3176e87f5c8ab8
nikoletaogoryalkova <nikoletaogoryalkova@gmail.com>
2018-06-13 18:59:27 +0300

    edit-section-services, added basic section for CAREERS

--- event.js
+++ event.js
@@ -7,6 +7,10 @@ function ClickWeb() {
         by using web applications. 
         We have specialized in creating web applications of any scale capable of serving to any amount of users simultaneously. From simple informational
          web sites (e.g. landing pages, business web sites) to complex applications running in the web browser (e.g. ERP, CRM). `);
+         $('#web-img').css("opacity","1");
+         $('#mobile-img').css("opacity","0.5");
+         $('#blockchain-img').css("opacity","0.5");
+         $('#consultancy-img').css("opacity","0.5");
 
     })
 };
@@ -19,6 +23,10 @@ function ClickMobile() {
          more than anything else. If something can run on your mobile phone, it will eventually run.
         Creating any service or informational application for mobile is our "Good morning!" phrase. That’s why it is very important to us to be able to run 
         in one and the same way on all of the major mobile operating systems.`);
+        $('#mobile-img').css("opacity","1");
+        $('#web-img').css("opacity","0.5");
+        $('#blockchain-img').css("opacity","0.5");
+        $('#consultancy-img').css("opacity","0.5");
         
     })
 };
@@ -31,7 +39,10 @@ function ClickBlockchain() {
         But the way we work removes the need for a middleman, leaving just the peer to peer communication. 
         Creating decentralized applications, smart contracts or running a new blockchain network are just a few of the services we offer in this area. 
         We have proven our skills by creating the biggest platform in the travelling industry purely relying on blockchain operations.`);
-        
+        $('#blockchain-img').css("opacity","1");
+        $('#web-img').css("opacity","0.5");
+        $('#mobile-img').css("opacity","0.5");
+        $('#consultancy-img').css("opacity","0.5");
 
     })
 };
@@ -44,7 +55,10 @@ function ClickConsultancy() {
         Are they facing showstoppers? Or their power is not enough for the project's needs? We are here to not only solve all of these problems 
         but also many more others that may arise. We can plug into an existing team and solve organizational or technical problems by providing innovative
         solutions. We can follow the existing process and suggest improvements, because we believe in the procedural way of working.`);
-        
+        $('#consultancy-img').css("opacity","1");
+        $('#web-img').css("opacity","0.5");
+        $('#mobile-img').css("opacity","0.5");
+        $('#blockchain-img').css("opacity","0.5");
     })
 };
 
