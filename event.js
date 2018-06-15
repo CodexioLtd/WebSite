2c63d34eeb7ea53c4bafa1cffe9f3c7422b0b838
nikoletaogoryalkova <nikoletaogoryalkova@gmail.com>
2018-06-15 16:21:03 +0300

    Edited project-images-popup

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
 function changeAllOpacities(currentElement) {
-    
+
     $('.service-img-icon').css('opacity', '0.5');
     $(currentElement).find("img").css('opacity', '1');
 }
@@ -17,19 +17,19 @@ $(".service-container").hover(e => setServiceDescription(
 );
 
 $(document).ready(() => {
-  var waypoint = new Waypoint({
-      element: document.getElementById('info-codexio'),
-      handler: function (direction) {
-          if (direction === 'down') {
-              $('.header-nav-bar').addClass('sticky');
-              $('.header-nav-bar').removeClass('sticky-header');
-          } else {
-              $('.header-nav-bar').removeClass('sticky');
-              $('.header-nav-bar').addClass('sticky-header');
-          }
-      },
-      offset: 100
-  });
+    var waypoint = new Waypoint({
+        element: document.getElementById('info-codexio'),
+        handler: function (direction) {
+            if (direction === 'down') {
+                $('.header-nav-bar').addClass('sticky');
+                $('.header-nav-bar').removeClass('sticky-header');
+            } else {
+                $('.header-nav-bar').removeClass('sticky');
+                $('.header-nav-bar').addClass('sticky-header');
+            }
+        },
+        offset: 100
+    });
     var waypoint = new Waypoint({
         element: document.getElementById('header'),
         handler: function (direction) {
@@ -43,29 +43,18 @@ $(document).ready(() => {
     });
 });
 
-$(document).ready(function() {
-	$('.zoom-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
-		
-	});
+
+$(function () {
+    "use strict";
+
+    $(".popup img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+
 });
